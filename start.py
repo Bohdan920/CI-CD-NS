30c493345635c86d35ff4735ecb77c8776becdc6
Bohdan Zakharchuk <Bohdan92@rivne.itstep.org>
2020-09-06 16:36:23 +0300

    refactor

--- start.py
+++ start.py
@@ -1,13 +1,12 @@
-import requests
-URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+  
+from lib.Currency import Currency
+import time
 
-def get_data(URL):
-    responce = requests.get(URL)
-    data = responce.json()
-    show_currency(data)
-def show_currency(currency):
-    for item in currency:
-        print(item["ccy"] + " " + item["base_ccy"]  + " " + item["buy"] + " | " + item["sale"]
-        )
+currency = Currency()
 
-get_data(URL)
+counter = 0
+while True:
+    counter+=1
+    print("Get online data => ", counter, " times")
+    currency.start()
+    time.sleep(300)
