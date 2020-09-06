d6c53ceab2f67e3fa0fcaa4bf327d1c109297fe0
Bohdan Zakharchuk <Bohdan92@rivne.itstep.org>
2020-09-06 16:57:02 +0300

    refactor

--- lib/Settings.py
+++ lib/Settings.py
@@ -2,4 +2,4 @@ if __name__ == "__main__":
     pass
 
 FILENAME = "currency.txt"
-URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"s
+URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
